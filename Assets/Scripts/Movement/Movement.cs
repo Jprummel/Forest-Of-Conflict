d7f31c7fbea7423697f34ac9a5a32834e51fc6f2
Jprummel <jordiprummel@live.nl>
2016-11-11 14:51:15 +0100

    Small clean up and fix

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -8,14 +8,12 @@ public class Movement : MonoBehaviour
     [SerializeField]private float _movementSpeed;
     [SerializeField]private float _turnSpeed;
     //Vectors
-    private Vector3 _newForward;
     private Vector3 _moveDir;
     private Quaternion _rotation;
     private CameraController _camController;
 
     void Start()
     {
-        //_rigidBody = GetComponent<Rigidbody>();
         _camController = GetComponent<CameraController>();
         _charController = GetComponent<CharacterController>();
     }
@@ -25,10 +23,6 @@ public class Movement : MonoBehaviour
         if (_moveDir.magnitude > 0)
         {
             Move();
-            if (_moveDir.sqrMagnitude > 1)
-            {
-                _newForward = _newForward.normalized;
-            }
         }
     }
 
@@ -39,17 +33,17 @@ public class Movement : MonoBehaviour
 
     public void Move()
     {
-        _moveDir = (_moveDir.x * _camController.CameraRight + _moveDir.z * _camController.CameraForward).normalized * _movementSpeed * Time.deltaTime;        
-
         float moveDirX = _moveDir.x >= 0 ? _moveDir.x : -_moveDir.x;
         float moveDirZ = _moveDir.z >= 0 ? _moveDir.z : -_moveDir.z;
         float maxSpeed = moveDirX >= moveDirZ ? moveDirX : moveDirZ;
-        
+
+        _moveDir = (_moveDir.x * _camController.CameraRight + _moveDir.z * _camController.CameraForward).normalized * _movementSpeed * maxSpeed * Time.deltaTime;
+
         if (_moveDir != Vector3.zero)
         {
             _rotation = Quaternion.LookRotation(_moveDir * Time.deltaTime);
         }
         this.transform.rotation = Quaternion.Lerp(this.transform.rotation,_rotation,Time.deltaTime * _turnSpeed);
-        _charController.Move(_moveDir * (_movementSpeed * maxSpeed) * Time.deltaTime);
+        _charController.Move(_moveDir);
     }
 }
